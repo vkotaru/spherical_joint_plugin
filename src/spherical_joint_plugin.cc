7c4ab9ae8aa64ad22df03b69e04c27fbc79f69a1
vkotaru <17078723+vkotaru@users.noreply.github.com>
2020-12-26 05:15:09 -0800

    Add S2 Integration and child_link update

--- src/spherical_joint_plugin.cc
+++ src/spherical_joint_plugin.cc
@@ -46,32 +46,93 @@ void SphericalJointPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) 
       boost::bind(&SphericalJointPlugin::OnUpdate, this, _1));
 
   // Initial pose to parent (primary) link
-  parent_reset_pose_ = parent_link_->WorldCoGPose();
-  child_reset_pose_ = child_link_->WorldCoGPose();
-
-  // Initial state
-  //  state_.q = vectorIgnition2Vector(parent_reset_pose_.Pos());
-  //  state_.w = vectorIgnition2Vector()
+  parent_reset_pose_ = parent_link_->WorldPose();
+  child_reset_pose_ = child_link_->WorldPose();
+  SphericalJointPlugin::CustomInit();
 
   gzmsg << "[SphericalJointPlugin] plugin loaded!" << std::endl;
 }
 
+void SphericalJointPlugin::CustomInit() {
+
+  gzmsg << "parent_initial_pose_ " << parent_reset_pose_ << std::endl;
+  gzmsg << "child_reset_pose_ " << child_reset_pose_ << std::endl;
+  pos_rel2parent_ = vectorIgnition2Eigen(child_link_->RelativePose().Pos());
+
+  // access the mass of the child_link
+  m = child_link_->GetInertial()->Mass();
+
+  // computing the link-orientation
+  IgnitionPose child_COG_pose_ = child_link_->WorldCoGPose();
+  Eigen::Vector3d rho = vectorIgnition2Eigen(child_COG_pose_.Pos()) - vectorIgnition2Eigen(child_reset_pose_.Pos());
+  l = rho.norm();
+
+  // Initial state
+  state_ = S2State(rho, Eigen::Vector3d::Zero());
+  gzmsg << "S2State " << state_ << std::endl;
+
+  // Initial angular acceleration
+  angular_accel_ = compute_angular_accel();
+
+  // Last time update
+  lastUpdateTime_ = world_->SimTime();
+}
+
 void SphericalJointPlugin::OnUpdate(const common::UpdateInfo &_info) {
-  IgnitionPose newPose;
-//  gzmsg << "[Debug] " << newPose << std::endl;
-//  child_link_->SetWorldPose(newPose);
+
+  currentUpdateTime_ = world_->SimTime();
+  double h = (currentUpdateTime_ - lastUpdateTime_).Double();
+
+  // Variational Integration on S2
+  Eigen::Vector3d dq, qnew, wnew, ang_vel_update;
+  Eigen::Matrix3d rotq = (hat(state_.w() * h)).exp();
+  dq = state_.w().cross(state_.q());
+  qnew = rotq * state_.q();
+  wnew = state_.w() + h * angular_accel_; // TODO verify this update against T.Lee et. al paper
+
+  // TODO add T.Lee paper reference
+
+  //  ang_vel_update = h * state_.w() + 0.5 * h * h * l * l * angular_accel_;
+  //  qnew = (ang_vel_update).cross(state_.q()) + sqrt(1 - pow(ang_vel_update.norm(), 2)) * state_.q();
+  //  wnew = state_.w() + h * l * l * angular_accel_;
+  state_.update(qnew, wnew);
+
+  // Angular Acceleration
+  angular_accel_ = compute_angular_accel();
+
+  // Set the new Pose & Velocity
+  IgnitionPose new_pose;
+  IgnitionVector lin_vel, ang_vel;
+
+  Eigen::Vector3d new_child_pos_W = vectorIgnition2Eigen(parent_link_->WorldPose().Pos())
+      + rotationIgnition2Eigen(parent_link_->WorldPose().Rot()) * pos_rel2parent_;
+  Eigen::Matrix3d new_child_rot_W = rotq * rotationIgnition2Eigen(child_link_->WorldPose().Rot());
+
+  new_pose = IgnitionPose(vectorEigen2Ignition(new_child_pos_W), rotationEigen2Ignition(new_child_rot_W));
+  lin_vel = vectorEigen2Ignition(l * dq);
+  ang_vel = vectorEigen2Ignition(wnew);
+  child_link_->SetWorldPose(new_pose);
+  child_link_->SetWorldTwist(lin_vel, ang_vel);
+
+  // Update the time variable
+  lastUpdateTime_ = currentUpdateTime_;
 }
 
 void SphericalJointPlugin::Reset() {
-  // Reset the parent link
+  // Reset the parent & child link
   parent_link_->SetWorldPose(parent_reset_pose_);
-  parent_link_->ResetPhysicsStates();
-  // Reset the parent link
   child_link_->SetWorldPose(child_reset_pose_);
+  parent_link_->ResetPhysicsStates();
   child_link_->ResetPhysicsStates();
+
+  SphericalJointPlugin::CustomInit();
 
   gzdbg << "[SphericalJointPlugin] reset!" << std::endl;
 }
+
+Eigen::Vector3d SphericalJointPlugin::compute_angular_accel() {
+  return -1 * (g / l) * state_.q().cross(e3);
+};
 
 GZ_REGISTER_MODEL_PLUGIN(SphericalJointPlugin);
 }
--- src/spherical_joint_plugin.hh
+++ src/spherical_joint_plugin.hh
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <gazebo/common/CommonTypes.hh>
 #include <eigen3/Eigen/Dense>
+#include <eigen3/unsupported/Eigen/MatrixFunctions>
 
 using IgnitionVector = ignition::math::Vector3d;
 using IgnitionPose = ignition::math::Pose3d;
@@ -15,7 +16,7 @@ using IgnitionQuaternion = ignition::math::Quaterniond;
 
 namespace gazebo {
 
-inline Eigen::Vector3d vectorIgnition2Vector(IgnitionVector vec) {
+inline Eigen::Vector3d vectorIgnition2Eigen(IgnitionVector vec) {
   Eigen::Vector3d out;
   out << vec.X(), vec.Y(), vec.Z();
   return out;
@@ -26,10 +27,65 @@ inline IgnitionVector vectorEigen2Ignition(Eigen::Vector3d vec) {
   return out;
 }
 
-inline Eigen::Matrix3d rotationIgnition2Vector(IgnitionQuaternion quat) {
+inline Eigen::Matrix3d rotationIgnition2Eigen(IgnitionQuaternion quat) {
   Eigen::Quaterniond eig_quat(quat.W(), quat.X(), quat.Y(), quat.Z());
   return eig_quat.toRotationMatrix();
 }
+
+inline IgnitionQuaternion rotationEigen2Ignition(Eigen::Matrix3d  rot) {
+  Eigen::Quaterniond q(rot);
+  IgnitionQuaternion ign_q(q.w(), q.x(), q.y(), q.z());
+  return ign_q;
+}
+
+inline Eigen::Matrix3d hat(const Eigen::Vector3d _vec) {
+  return (Eigen::Matrix3d() << 0.0, -_vec(2), _vec(1), _vec(2),
+      0.0, -_vec(0), -_vec(1), _vec(0), 0.0)
+      .finished();
+}
+
+/// \brief Spherical joint state on $S^2$ manifold
+class S2State {
+ public:
+  /// \brief Default constructor with q=e3 and w=zero(3,1)
+  S2State() : q_((Eigen::Vector3d() << 0, 0, 1).finished()), w_(Eigen::Vector3d::Zero()) {}
+  /// \brief Constructor with a vector and angular velocity
+  /// vector will normalize in constructor
+  /// \param: _q Eigen::Vector3d q in R^3
+  /// \param: _w Eigen::Vector3d w in R^3
+  S2State(Eigen::Vector3d _q, Eigen::Vector3d _w) : q_(_q), w_(_w) {
+    q_.normalize();
+  }
+ public:
+  friend std::ostream &operator<<(
+      std::ostream &_out, const S2State &_state) {
+    _out << "q: " << ignition::math::precision(_state.q()[0], 6)
+         << " " << ignition::math::precision(_state.q()[1], 6)
+         << " " << ignition::math::precision(_state.q()[2], 6)
+         << " w: " << ignition::math::precision(_state.w()[0], 6)
+         << " " << ignition::math::precision(_state.w()[1], 6)
+         << " " << ignition::math::precision(_state.w()[2], 6);
+    return _out;
+  }
+  /// \brief Public access to the state q_
+  Eigen::Vector3d q() const {
+    return q_;
+  }
+  /// \brief Public access to the state w_
+  Eigen::Vector3d w() const {
+    return w_;
+  }
+  void update(Eigen::Vector3d _q, Eigen::Vector3d _w) {
+    _q.normalize();
+    q_ = _q;
+    w_ = _w;
+  }
+ private:
+  /// \brief unit-vector q on (S2)
+  Eigen::Vector3d q_;
+  /// \brief angular velocity w on (TS2)
+  Eigen::Vector3d w_;
+};
 
 class SphericalJointPlugin : public ModelPlugin {
 
@@ -38,11 +94,14 @@ class SphericalJointPlugin : public ModelPlugin {
   ~SphericalJointPlugin() override;
 
  protected:
+  void CustomInit();
   void Reset() override;
   void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) override;
   void OnUpdate(const common::UpdateInfo &_info);
 
  private:
+  common::Time lastUpdateTime_, currentUpdateTime_;
+
   std::string parent_link_name_;
   std::string child_link_name_;
   std::string namespace_;
@@ -61,15 +120,13 @@ class SphericalJointPlugin : public ModelPlugin {
   /// \brief Reset the link pose
   IgnitionPose parent_reset_pose_, child_reset_pose_;
 
-  /// \brief Spherical joint state on $S^2$ manifold
-  struct S2State {
-    S2State() : q((Eigen::Vector3d()<< 0, 0, 1).finished()), w(Eigen::Vector3d::Zero()) {}
-    /// \brief unit-vector q on (S2)
-    Eigen::Vector3d q;
-    /// \brief angular velocity w on (TS2)
-    Eigen::Vector3d w;
-  };
   S2State state_;
+  double l, m, g = 9.81;
+  Eigen::Vector3d e3 = (Eigen::Vector3d() << 0., 0., 1.).finished();
+  Eigen::Vector3d angular_accel_ = Eigen::Vector3d::Zero();
+  Eigen::Vector3d pos_rel2parent_;
+
+  Eigen::Vector3d compute_angular_accel();
 
 };
 
